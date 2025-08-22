0092b23f9dd80ac1126a248cd5db6d749b0b0e6c
Kavya Kaira <kavyakaira2005@gmail.com>
2025-08-22 23:31:34 +0530

    Final Currency Converter

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ let baseUrl = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/
 let dropdowns = document.querySelectorAll(".dropdown select")
 let btn = document.querySelector("form button")
 
+let select = document.querySelector(".dropdown select")
+let msg = document.querySelector(".msg")
+let icon = document.querySelector("#swap-btn")
 
 
 for(let select of dropdowns){
@@ -32,7 +35,7 @@ const updateFlag = (ele) => {
     img.src = newSrc
 }
 
-btn.addEventListener("click", (evt) => {
+btn.addEventListener("click", async (evt) => {
     evt.preventDefault()
     let amt = document.querySelector(".amount input")
     let amtVal = amt.value
@@ -41,9 +44,11 @@ btn.addEventListener("click", (evt) => {
         amt.value = "1"
     }
 
-    
+    let fromCurr = document.querySelector("select[name = 'from']").value.toLowerCase()
+    let toCurr = document.querySelector("select[name = 'to']").value.toLowerCase()
 
-    console.log(convertCurr(amt.value, ))
+    let convertedAmt = await convertCurr(amt.value, fromCurr, toCurr)
+    msg.innerText = `${amtVal}${fromCurr.toUpperCase()} = ${convertedAmt}${toCurr.toUpperCase()}`
 })
 
 async function convertCurr(amt, from, to){
@@ -51,7 +56,23 @@ async function convertCurr(amt, from, to){
     const res = await fetch(url)
     const data = await res.json()
     const rate = data[from][to]
-    return amt*rate
+    return amt*rate.toFixed(2)
+}
+
+icon.addEventListener("click", () => {
+    let from = document.querySelector("select[name = 'from']")
+    let to = document.querySelector("select[name = 'to']")
+
+    swap(from, to)
+})
+
+function swap(from, to){
+    let temp = from.value
+    from.value = to.value
+    to.value = temp
+
+    updateFlag(from)
+    updateFlag(to)
 }
 
 
